a9063ab89efc30ae7f6ea1f9899233333b65a589
kimseeun <cocobell3@naver.com>
2024-02-25 16:46:33 +0900

    feat: typeORM postgreSQL연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/groom')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/groom'),
+    TypeOrmModule.forRoot({
+      // 데이터 베이스 타입
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'groomPreTest',
+      //개발환경에서는 true
+      //production한경에서는 false
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
