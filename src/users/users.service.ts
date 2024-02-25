0da50fffb67a889473a94f158c5c7ecb13926a8e
kimseeun <cocobell3@naver.com>
2024-02-25 18:11:33 +0900

    feat: 회원가입에 사용할 createUser함수 구현

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersModel } from './entities/users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersModel)
+    private readonly usersRepository: Repository<UsersModel>,
+  ) {}
+
+  //회원가입시 사용할 로직
+  async createUser(user: Pick<UsersModel, 'email' | 'nickname' | 'password'>) {
+    const nicknameExists = await this.usersRepository.exists({
+      where: { nickname: user.nickname },
+    });
+    if (nicknameExists) {
+      throw new BadRequestException('이미 존재하는 nickname입니다!');
+    }
+    const emailExists = await this.usersRepository.exists({
+      where: { email: user.email },
+    });
+    if (emailExists) {
+      throw new BadRequestException(`${user.email}로 가입한 이력이 있습니다.`);
+    }
+    const userObject = this.usersRepository.create({
+      nickname: user.nickname,
+      email: user.email,
+      password: user.password,
+    });
+
+    const newUser = await this.usersRepository.save(userObject);
+
+    return newUser;
+  }
+}
